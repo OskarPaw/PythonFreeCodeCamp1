5dc6a68732b8885950de571197b9b4bb43caed57
OskarPaw <95379638+OskarPaw@users.noreply.github.com>
2021-12-25 20:11:03 +0100

    Multi-Byte Characters

--- Python Intro (podstawy).py
+++ Python Intro (podstawy).py
@@ -965,3 +965,43 @@ print(xyz)
 # it is the east and Juliet is the sun
 # arise fair sun and kill the envious moon
 # who is already sick and pale with grief
+
+## Characters and Strings
+# ASCII - american Standard Code for Information Interchange
+# each character is represented by a number between 0 and 256 stored in 8 bits of memory
+# we refer to "8 bits of memory as a "byte" of memory – (i.e. my disk drive contains 3 Terabytes of memory)
+# the ord() function tells us the numeric value of a simple ASCII character
+# print(ord('H')) # 72
+# print(ord('e')) # 101
+
+# Multi-Byte Characters
+# to represent the wide range of characters computers must handle we represent characters with more than one byte
+# UTF-16 – Fixed length - Two bytes
+# UTF-32 – Fixed Length - Four Bytes
+# UTF-8 – 1-4 bytes # best one due to being dynamic and having data compression
+#       -  Upwards compatible with ASCII
+#       -  Automatic detection between ASCII and UTF-8
+#       -  UTF-8 is recommended practice for encoding data to be exchanged between systems
+
+# Python 3 and Unicode
+# in Python 3, all strings internally are UNICODE
+# working with string variables in Python programs and reading data from files usually "just works"
+# when we talk to a network resource using sockets or talk to a database we have to encode and decode data (usually to UTF-8
+# when we talk to an external resource like a network socket we send bytes, so we need to encode Python 3 strings into a given character encoding
+# when we read data from an external resource, we must decode it based on the character set so it is properly represented in Python 3 as a string
+
+# An HTTP Request in Python
+# import socket
+#
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect(('data.pr4e.org', 80))
+# cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode() # takes unicode and makes it into UTF bytes
+# mysock.send(cmd)
+#
+# while True:
+#     data = mysock.recv(512)
+#     if (len(data) < 1):
+#         break
+#     print(data.decode())
+# mysock.close()
+
