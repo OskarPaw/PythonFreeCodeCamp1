3220c3a60f442db8b7e6b3c2b45c5880e9ae8196
OskarPaw <95379638+OskarPaw@users.noreply.github.com>
2021-12-26 22:30:40 +0100

    Making HTTP Easier With urllib

--- Python Intro (podstawy).py
+++ Python Intro (podstawy).py
@@ -1005,3 +1005,26 @@ print(xyz)
 #     print(data.decode())
 # mysock.close()
 
+# Making HTTP Easier With urllib
+# using urllib in Python
+# since HTTP is so common, we have a library that does all the socket work for us and makes web pages look like a file
+# import urllib.request, urllib.parse, urllib.error
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# for line in fhand:
+#     print(line.decode().strip()) # its getting printed without headers
+
+# Its like a file if we use urllib
+# import urllib.request, urllib.parse, urllib.error
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# counts = dict()
+# for line in fhand:
+#     words = line.decode().split()
+#     for word in words:
+#         counts[word] = counts.get(word, 0) + 1
+# print(counts)
+
+# We can also follow links
+# import urllib.request, urllib.parse, urllib.error
+# fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+# for line in fhand:
+#     print(line.decode().strip())
