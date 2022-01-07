7d1fc1b92e21fbf612eedea31415d30fe53ea93f
OskarPaw <95379638+OskarPaw@users.noreply.github.com>
2022-01-07 18:51:04 +0100

    Relational Databases and SQLite

--- Python Intro (podstawy).py
+++ Python Intro (podstawy).py
@@ -1405,3 +1405,41 @@ print(xyz)
 # Summary
 # Object Oriented programming is a very structured approach to code reuse
 # we can group data and functionality together and create many independent instances of a class
+
+#                                                                                         Relational Databases and SQLite
+# Relational Databases
+# relational databases model data by storing rows and columns in tables.
+# the power of the relational database lies in its ability to efficiently retrieve data from those tables and in particular where there are multiple tables and the relationships between those tables involved in the query.
+
+# Terminology
+# database - contains many tables
+# relation (or table) - contains tuples and attributes
+# tuple (or row) - a set of fields that generally represents an “object” like a person or a music track
+# attribute (also column or field) - one of possibly many elements of data corresponding to the object represented by the row
+
+# Relation
+# is defined as a set of tuples that have the same attributes.
+# a tuple usually represents an object and information about that object.  Objects are typically physical objects or concepts.
+# a relation is usually described as a table, which is organized into rows and columns.
+# all the data referenced by an attribute are in the same domain and conform to the same constraints.
+
+# SQL
+# Structured Query Language is the language we use to issue commands to the database
+# -  create data (a.k.a Insert)
+# -  retrieve data
+# -  update data
+# -  delete data
+
+# Web Applications w/ Databases
+# application Developer - Builds the logic for the application, the look and feel of the application - monitors the application for problems
+# database Administrator - Monitors and adjusts the database as the program runs in production
+# often both people participate in the building of the “Data model”
+# a database model or database schema is the structure or format of a database, described in a formal language supported by the database management system. In other words, a “database model” is the application of a data model when used in conjunction with a database management system.
+
+# Common Database Systems
+# three major Database Management Systems in wide use
+# -  Oracle - Large, commercial, enterprise-scale, very very tweakable
+# -  MySql - Simpler but very fast and scalable - commercial open source
+# -  SqlServer - Very nice - from Microsoft (also Access)
+# many other smaller projects, free and open source
+# -  HSQL, SQLite, Postgres
