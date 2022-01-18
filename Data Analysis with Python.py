1a8211e00522346398bb4ce6b35ddc289368f82d
OskarPaw <95379638+OskarPaw@users.noreply.github.com>
2022-01-18 20:19:01 +0100

    Add files via upload

---
+++ Data Analysis with Python.py
@@ -0,0 +1,58 @@
+#                                                                   Data Analysis with Python
+# What is Data Analysis
+# a process of inspecting, cleansing, transforming and modeling data with the goal of discovering useful information
+# informing conclusion and supporting decision-making.
+
+# The Data Analysis Process
+# Data Extraction ------> Data Cleaning -------------------> Data Wrangling -------------------------> Analysis ------------------------------> Action
+# SQL                    # Missing values and empty data   # Hierarchical Data                      # Exploration                       # Building Machine Learning Models
+# Scrapping              # Data imputation                 # Handling categorical data              # Building statistical models       # Feature Engineering
+# File Formats           # Incorrect types                 # Reshaping and transforming structures  # Visualization and representations # Moving ML into production
+# - CSV
+# - JSON
+# - XML
+# Consulting APIs        # Incorrect or invalid values     # Indexing data for quick access         # Correlation vs Causation analysis # Building ETL pipelines
+# Buying Data            # Outliers and non relevant data  # Merging, combining and joining data    # Hypothesis testing                # Live dashboard and reporting
+# Distributed Databases  # Statistical sanitization                                                 # Statistical analysis              # Decision making and real-life tests
+                                                                                                    # Reporting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
