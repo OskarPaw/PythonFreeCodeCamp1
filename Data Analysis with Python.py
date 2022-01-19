9ef8e8dd549e5db54d47b311a0918811500eaf09
OskarPaw <95379638+OskarPaw@users.noreply.github.com>
2022-01-19 19:41:50 +0100

    Update Data Analysis with Python.py

--- Data Analysis with Python.py
+++ Data Analysis with Python.py
@@ -16,20 +16,65 @@
 # Distributed Databases  # Statistical sanitization                                                 # Statistical analysis              # Decision making and real-life tests
                                                                                                     # Reporting
 
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
+# %matplotlib inline
+# !head data/file_name.csv  # opening file
+# sales = pd.read_csv(      # reading file
+#   'data/sales_data.csv',
+#    parse_dates=['Date'])
 
+# Checking data look 
+# filename.head()
+# filename.shape
+# filename.info()
+# filename.describe()
 
+# sales['Calculated_Cost'] = sales['Order_Quantity'] * sales['Unit_Cost']
 
+# sales['Calculated_Cost'].head()
+# (sales['Calculated_Cost'] != sales['Cost']).sum() # checking if there are diffrences 
 
+# We can see the relationship between Cost and Profit using a scatter plot:
+# sales.plot(kind='scatter', x='Calculated_Cost', y='Profit', figsize=(6,6))
 
+#Calculated_Revenue = Cost + Sales
+# sales['Calculated_Revenue'] = sales['Cost'] + sales['Profit']
 
+#sales['Calculated_Revenue'].head()
 
+# (sales['Calculated_Revenue'] != sales['Revenue']).sum() # checking diff
 
+# Modifying price by adding diff %:
+# sales['Unit_Price'].head()
+# 0    120
+# 1    120
+# 2    120
+# 3    120
+# 4    120
+# Name: Unit_Price, dtype: int64
 
+# sales['Unit_Price'] *= 1.03
+# sales['Unit_Price'].head()
+# 0    123.6
+# 1    123.6
+# 2    123.6
+# 3    123.6
+# 4    123.6
+# Name: Unit_Price, dtype: float64
 
+# Get the mean revenue of the specific sales group
+#sales.loc[sales['Age_Group'] == 'specific age group', 'Revenue'].mean()
 
+# How many records belong to Age Group x or Adults y?
+# sales.loc[(sales['Age_Group'] == 'x') | (sales['Age_Group'] == 'y')].shape[0]
 
-
+# Increase the revenue by specific % to every sale made in specific country
+# sales.loc[sales['Country'] == 'specific country', 'Revenue'] *= 1.1
+# sales.loc[sales['Country'] == 'specific country', 'Revenue'].head()
+# 5min.
 
 
 
